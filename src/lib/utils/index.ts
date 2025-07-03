305705595937c4814489e866c7050de858cf87a5
maxli <maxli@hemlock.com>
2025-07-03 16:28:01 -0700

    test

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -60,21 +60,32 @@ export function clickOutside(node: HTMLElement, handler: () => void): { destroy:
 	};
 }
 
-export function saveObjectToLocalStorage<T>(key: string, object: T) {
-	localStorage.setItem(key, JSON.stringify(object));
+import type { Cookies } from '@sveltejs/kit';
+
+export function saveObjectToCookies<T>(
+	cookies: Omit<Cookies, 'getAll' | 'delete' | 'serialize' | 'get'>,
+	key: string,
+	object: T
+) {
+	cookies.set(key, JSON.stringify(object), { path: '/' });
 }
-export function loadObjectFromLocalStorage<T>(key: string): T | null {
-	const saved = localStorage.getItem(key);
+export function loadObjectFromCookies<T>(cookies: Cookies, key: string): T | null {
+	const saved = cookies.get(key);
 	if (saved) {
 		try {
 			return JSON.parse(saved);
 		} catch (e) {
-			console.error('Invalid object in localStorage', e);
+			console.error('Invalid object in cookies', e);
 		}
 	}
 	return null;
 }
 
-export function clearObjectFromLocalStorage(key: string) {
-	localStorage.removeItem(key);
-}
+export let clientCookies: Omit<Cookies, 'getAll' | 'delete' | 'serialize' | 'get'> = {
+	set: (name: string, value: string) => {
+		const date = new Date();
+		date.setTime(date.getTime() + 24 * 60 * 60 * 100000000); // 100 thousand days
+		const expires = 'expires=' + date.toUTCString();
+		document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}; path=/`;
+	}
+};
---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,7 @@
+export const load = async ({ cookies }) => {
+	let authenticated = save;
+
+	return {
+		authenticated
+	};
+};
