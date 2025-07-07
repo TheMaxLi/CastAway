432195f8bf7d72db4137aaf7444cb82e2feae1b0
maxli <maxli@hemlock.com>
2025-07-07 16:56:16 -0700

    fix timestamp

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,10 @@
+import { sql } from 'drizzle-orm';
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const post = sqliteTable('post', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
 	image: text().notNull(),
-	createdAt: integer({ mode: 'timestamp' }).default(new Date()),
+	createdAt: integer({ mode: 'timestamp' }).default(sql`(unixepoch())`),
 	postedBy: text({ enum: ['max_monkey', 'tina_monkey'] }).notNull(),
 	latitude: integer(),
 	longitude: integer()
