d09f7ec4be0170971eb227acb68e79b6cc2faa6a
maxli <maxli@hemlock.com>
2025-07-17 15:59:07 -0700

    change background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	style="background-image: url(/background.JPG); background-size: cover;"
+	style="background-image: url(/cloud_background.png); background-size: 100% 100%;"
 	class="h-screen w-full"
 >
 	{@render children()}
